7bb9c2bc5c28b8edd91845137e236bb99d646996
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-11-21 09:38:50 -0300

    01.005_Aplicacao_Inicial

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -23,9 +23,9 @@ def cadastrar_cliente():
         except:
             print("Cliente não cadastrado")
 
-    return render_template("clientes/cadastrar_cliente.html")
+    return
 
 @app.route("/listar_clientes", methods=["GET"])
 def listar_clientes():
     clientes = cliente_service.listar_clientes()
-    return render_template("clientes/lista_clientes.html")
+    return
